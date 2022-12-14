2965a5e3594e69b3308f1c6fcccd65888018330a
jerzy-vletter <vletter123@gmail.com>
2022-12-14 19:02:01 +0100

    day 1 complete

--- day-1.py
+++ day-1.py
@@ -1,20 +1,37 @@
-inp = [int(x) for x in input("insert your int: ").split()]
-print(inp)
-elf = 1
+text = open('day-1-input.txt', 'r')
+setup = text.readlines()
 answer = 0
-"""
+inp = []
+out = []
+
+for line in setup:
+        try:
+            int_value = int(line)
+        except ValueError:
+            if line[-1] == '\n':
+                inp.append(line[:-1])
+            else:
+                inp.append(line)
+        else:
+            if line[-1] == '\n':
+                inp.append(int(line[:-1]))
+            else:
+                inp.append(int(line))
+
+
+
 for i, value in enumerate(inp):
-    end = len(inp)-19 
+    end = len(inp)-1
     try:
         int_value = int(value)
     except ValueError:
-        print("elf ", elf," is carrying ->", answer, " calories")
-        elf = elf+1
+        out.append(answer)
         answer = 0
     else:
         answer = answer+value
     if(i == end):
-        print("elf ", elf," is carrying ->", answer, " calories")
-        elf = elf+1
+        out.append(answer)
         answer = 0
-"""
+
+answer = max(out)
+print(answer)
